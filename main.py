acbd956b5b45a2b2b871664f7d310640cda8bff3
0xyukine <nesryn@protonmail.com>
2022-06-26 21:00:33 +0100

    Actually pushing the modifications in the relevant scripts

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from randomkana import RandomKana
 
 rk = RandomKana()
-print(rk.random())
+print("(h)iragana, (k)atakana, or (b)oth: ", end="")
+choice = input()
+if choice in {"h","k","b"}:
+	while True:
+		if choice == "h":
+			r = rk.hira()		#Random hiragana
+		elif choice == "k":
+			r = rk.kata()		#Random katakana
+		elif choice == "b":
+			r = rk.kana()		#Random kana
 
-while True:
-	r = rk.random()
-	print(r[0])
-	i = input()
-	if i == r[1][0]:
-		print("Correct")
-	else:
-		print(f"False, correct answer is: {r[1][0]}")
+		print(r[0])				#Prints the kana
+		i = input()
+		if i == r[1][0]:		#Tests user input against romanization
+			print("Correct")
+		else:
+			print(f"False, correct answer is: {r[1][0]}")
+else:
+	print("Invalid choice")
--- randomkana.py
+++ randomkana.py
@@ -2,10 +2,21 @@ import json
 import random
 
 class RandomKana:
+    #Load both kana text files into dictionaries on initialisation
     def __init__(self):
         with open("hiragana.txt") as f:
             self.hiragana = json.load(f)
+        with open("katakana.txt") as f:
+            self.katakana = json.load(f)
 
-    def random(self):
+    def hira(self):
         kana, eng = random.choice(list(self.hiragana.items()))
-        return (kana, eng)
+        return (kana,eng)
+
+    def kata(self):
+        kana, eng = random.choice(list(self.katakana.items()))
+        return (kana,eng)
+
+    def kana(self):
+        kana, eng = random.choice(list({**self.hiragana,**self.katakana}.items()))
+        return (kana,eng)
